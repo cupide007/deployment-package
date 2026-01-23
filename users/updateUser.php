84abff727d40b8e3c8e7e6b9d5909d55948f87b7
等风几许JX <92464802+cupide007@users.noreply.github.com>
2026-01-24 04:09:38 +0800

    Allow invalid JSON payloads for update user

--- users/updateUser.php
+++ users/updateUser.php
@@ -69,11 +69,8 @@ try {
         if ($trimmedInput !== '') {
             $updateData = json_decode($trimmedInput, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
-                http_response_code(400);
-                echo json_encode(['error' => '请提供要更新的数据'], JSON_UNESCAPED_UNICODE);
-                exit;
-            }
-            if ($updateData === null) {
+                $updateData = [];
+            } elseif ($updateData === null) {
                 $updateData = [];
             }
         } else {
