29958770271bf4c64c79942b79685a13dd02238e
DFJX <1539781486@qq.com>
2026-01-26 23:41:31 +0800

    feat:重构前端用户中心

--- common.php
+++ common.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-Session-Id');
-
+header("Content-Security-Policy: default-src 'self' https: data: blob: 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data: blob:; media-src 'self' https: data: blob:;");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
